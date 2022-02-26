9823f6f5f49d9485e807d8b1763571b93c843e20
Allename <allename@Anthonys-MacBook-Pro.local>
2022-02-26 19:03:27 +0100

    feat: update

--- lib/screens/onboarding/get_started.dart
+++ lib/screens/onboarding/get_started.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sylar/utils/constants.dart';
 import 'package:sylar/widgets/header.dart';
 
@@ -12,11 +13,54 @@ class GetStarted extends StatelessWidget {
         backgroundColor: kPrimary,
       ),
       body: Container(
-        padding: const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget> [
             Header(title: 'Get Started', description: 'Welcome to the world of easy transactions'),
+            SizedBox(height: 20.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget> [
+                const Text(
+                  'Full name',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                const TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
+                    fillColor: Colors.white,
+                    filled: false,
+                    hintText: 'Full Name',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                const Text(
+                  'Phone number',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                TextField(
+                  keyboardType: TextInputType.number,
+                   inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Your phone number',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
