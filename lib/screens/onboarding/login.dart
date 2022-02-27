8dbfa436fe4408b4217a8cdd9bdc56ea9ffa358b
Allename <allename@Anthonys-MacBook-Pro.local>
2022-02-27 21:15:31 +0100

    feat: update

--- lib/screens/onboarding/login.dart
+++ lib/screens/onboarding/login.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:sylar/utils/constants.dart';
 
 class Login extends StatelessWidget {
   const Login({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kPrimary,
+      ),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget> [
+            
+          ],
+        ),
+      ),
     );
   }
 }
