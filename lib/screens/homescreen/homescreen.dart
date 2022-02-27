e595e72bcfd6d2138be271f0795eae24309d0cda
Allename <allenameanthony00@gmail.com>
2022-02-28 00:05:15 +0100

    created homescreen

---
+++ lib/screens/homescreen/homescreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({ Key? key }) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+        child: Container(
+          child: Column(
+            children: <Widget> [
+              Stack(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
