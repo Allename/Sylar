71259b890e931c41c6731acada85132c3d722be9
Allename <allenameanthony00@gmail.com>
2022-02-28 00:35:41 +0100

    made changes to constants

---
+++ lib/screens/homescreen/widgets/wallet_balance.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:sylar/utils/constants.dart';
+
+class WalletBalance extends StatelessWidget {
+  final String text;
+  final String price;
+  const WalletBalance({ Key? key, required this.text, required this.price }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget> [
+          Text(
+            text,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+              color: kSecondary,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const kPrimary = Color(0xFF0066FF);
 const kSecondary = Color(0xFFCCE0FF);
+const kGray = Color(0xFFE5E5E5);
