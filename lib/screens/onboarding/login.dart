11955c6f9092a704e93922fc05b1db87ce6abe32
Allename <allename@Anthonys-MacBook-Pro.local>
2022-02-27 23:05:20 +0100

    added image to assets

--- lib/screens/onboarding/login.dart
+++ lib/screens/onboarding/login.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: unnecessary_string_escapes
+
 import 'package:flutter/material.dart';
 import 'package:sylar/utils/constants.dart';
+import 'package:sylar/widgets/button.dart';
+import 'package:sylar/widgets/header.dart';
 
 class Login extends StatelessWidget {
   const Login({ Key? key }) : super(key: key);
@@ -14,8 +18,76 @@ class Login extends StatelessWidget {
         width: double.infinity,
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget> [
-            
+            const Header(title: 'Login', description: 'Welcome back, it\’s a lovely day'),
+            const SizedBox(height: 33.0),
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget> [
+                  const Text(
+                    'Email',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  const TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'you@example.com',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 24.0),
+                  const Text(
+                    'Password',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  const TextField(
+                    autofocus: false,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Minimun of 8 characters',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  Button(text: 'Login', press: () {}),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget> [
+                      const Text(
+                        'Don\'t have an account?',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => const Login())),
+                        child: const Text(
+                          'Get Started',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
